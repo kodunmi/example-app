655fd5b1db71080a6d95e40b158554096cf80b3c
kodunmi <53086177+kodunmi@users.noreply.github.com>
2023-09-12 10:22:18 +0100

    remove db from scribe

--- config/scribe.php
+++ config/scribe.php
@@ -258,8 +258,7 @@ This documentation aims to provide all the information you need to work with our
 
 <aside>As you scroll, you'll see code examples for working with the API in different programming languages in the dark area to the right (or as part of the content on mobile).
 You can switch the language used with the tabs at the top right (or from the nav menu at the top left on mobile).</aside>
-INTRO
-    ,
+INTRO,
 
     /*
      * Example requests for each endpoint will be shown in each of these languages.
@@ -433,10 +432,10 @@ INTRO
     'routeMatcher' => \Knuckles\Scribe\Matching\RouteMatcher::class,
 
     /**
-     * For response calls, API resource responses and transformer responses,
-     * Scribe will try to start database transactions, so no changes are persisted to your database.
-     * Tell Scribe which connections should be transacted here.
-     * If you only use one db connection, you can leave this as is.
-     */
-    'database_connections_to_transact' => [config('database.default')]
+ * For response calls, API resource responses and transformer responses,
+ * Scribe will try to start database transactions, so no changes are persisted to your database.
+ * Tell Scribe which connections should be transacted here.
+ * If you only use one db connection, you can leave this as is.
+ */
+    // 'database_connections_to_transact' => [config('database.default')]
 ];
